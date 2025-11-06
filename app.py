11a9af860f7da87a5d0e5b49961659014191870b
jorisschwiezer-lab <joris.schwiezer@icloud.com>
2025-11-06 12:27:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 st.set_page_config(layout="wide", page_title="Tom Cruise Jet Tracker (2024)", page_icon="✈️")
 
 # Name der CSV-Datei (Daten aus 2024)
-CSV_FILE = 'Tom_Cruise_Jet_2024.csv'
+CSV_FILE = 'tom_cruise_n350xx_flights.csv'
 
 # Konstante für den CO2-Vergleich (Jährliche CO2-Emissionen der Vergleichsstadt)
 # Platzhalter: Jährlicher Ausstoß Ingolstadt in Tonnen CO₂
